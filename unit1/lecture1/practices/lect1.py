dae536f5c65b348b54c19150030dd58bdb0b0f15
Syen Park <syenpark@gmail.com>
2018-06-30 18:05:29 +0900

    complete lect1.py

--- unit1/lecture1/practices/lect1.py
+++ unit1/lecture1/practices/lect1.py
@@ -18,7 +18,7 @@ class Food(object):
         return self.calories
     
     def density(self):
-        return self.getValue() / self.getCost()
+        return self.get_value() / self.get_cost()
     
     def __str__(self):
         return self.name + ': <' + str(self.value)\
@@ -58,3 +58,21 @@ def test_greedy(items, constraint, key_function):
     
     for item in taken:
         print('   ', item)
+        
+def test_greedys(foods, max_units):
+    print('Use greedy by value to allocate', max_units, 'calories')
+    test_greedy(foods, max_units, Food.get_value)
+    
+    print('\nUse greedy by cost to allocate', max_units, 'calories')
+    test_greedy(foods, max_units, lambda x: 1/Food.get_cost(x))
+    
+    print('\nUse greedy by density to allocate', max_units, 'calories')
+    test_greedy(foods, max_units, Food.density)
+    
+names = ['wine', 'beer', 'pizza', 'burger', 'fries',
+         'cola', 'apple', 'donut', 'cake']
+values = [89,90,95,100,90,79,50,10]
+calories = [123,154,258,354,365,150,95,195]
+foods = build_menu(names, values, calories)
+test_greedys(foods, 1000)
+    
