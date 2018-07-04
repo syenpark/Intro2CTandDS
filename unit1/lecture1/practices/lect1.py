d2915701619e9d9410858845f4884f1d75825f69
Syen Park <syenpark@gmail.com>
2018-07-04 23:50:20 +0900

    add build memu

--- unit1/lecture1/practices/lect1.py
+++ unit1/lecture1/practices/lect1.py
@@ -24,3 +24,20 @@ class Food(object):
         return self.name + ': <' + str(self.value) \
                + ', ' + str(self.calories) + '>'
         
+def build_menu(names, values, calories):
+    menu = []
+    
+    for i in range(len(values)):
+        menu.append(Food(names[i], values[i], calories[i]))
+    
+    return menu
+
+def test_greedy():
+    names = ['wine', 'beer', 'pizza', 'burger', 'fries',
+         'cola', 'apple', 'donut', 'cake']
+    values = [89,90,95,100,90,79,50,10]
+    calories = [123,154,258,354,365,150,95,195]
+    
+    menu = build_menu(names, values, calories)
+    
+test_greedy()
