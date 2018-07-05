d0679d553d2d5e3f64cd8c485363e04379ddf3d6
Syen Park <syenpark@gmail.com>
2018-07-05 23:14:58 +0900

    add lecture2-segment1.py

---
+++ unit1/lecture2/practices/lecture2-segment1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+@auther: syenpark
+Python Version: 3.6
+"""
+class Food(object):
+    def __init__(self, name, value, calories):
+        self.name = name
+        self.value = value
+        self.calories = calories
+        
+    def get_value(self):
+        return self.value
+    
+    def get_cost(self):
+        return self.calories
+    
+    def get_density(self):
+        try:
+            return self.get_value() / self.get_cost()
+        except ZeroDivisionError:
+            return float('inf') 
+    
+    def __str__(self):
+        return self.name + ': <' + str(self.get_value()) \
+               + ', ' + str(self.get_cost()) + '>'
+    
+    
+
+names = ['wine', 'beer', 'pizza', 'burger', 'fries',
+         'cola', 'apple', 'donut', 'cake']
+values = [89,90,95,100,90,79,50,10]
+calories = [123,154,258,354,365,150,95,195]
