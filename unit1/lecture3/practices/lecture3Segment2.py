9f25d46aa394d08c57610dc65348803bce1ba031
Syen Park <syenpark@gmail.com>
2018-07-07 00:50:16 +0900

    add edge class

--- unit1/lecture3/practices/lecture3Segment2.py
+++ unit1/lecture3/practices/lecture3Segment2.py
@@ -17,3 +17,20 @@ class Node(object):
     
     def __str__(self):
         return self.name
+    
+class Edge(object):
+    def __init__(self, src, dest):
+        """
+        inputs: src, dest are node
+        """
+        self.src = src
+        self.dest = dest
+        
+    def get_source(self):
+        return self.src
+    
+    def get_destination(self):
+        return self.dest
+    
+    def __str__(self):
+        return self.src.getName() + '->' + self.dest.getName()
