d6d05002f7d11bc9866bd3e760e4a2f81899cd05
Syen Park <syenpark@gmail.com>
2018-07-15 19:32:37 +0900

    update

--- unit1/lecture3/practices/lecture3Segment2.py
+++ unit1/lecture3/practices/lecture3Segment2.py
@@ -8,90 +8,95 @@ Python Version: 3.6
 class Node(object):
     def __init__(self, name):
         """
-        name is a string
+        name should be a string
         """
-        self.name = name
-    
+        self.__name = name
+        
     def get_name(self):
-        return self.name
+        return self.__name
     
     def __str__(self):
-        return self.name
+        return self.__name
     
 class Edge(object):
     def __init__(self, src, dest):
-        self.src = src
-        self.dest = dest
+        """
+        src and dest are Node
+        """
+        self.__src = src
+        self.__dest = dest
         
     def get_src(self):
-        return self.src
+        return self.__src
     
     def get_dest(self):
-        return self.dest
+        return self.__dest
     
     def __str__(self):
-        return self.src.get_name() + '->' + self.dest.get_name()
+        return self.__src.get_name() + '->' + self.__dest.get_name()
     
 class Digraph(object):
+    """
+    build digraction graph
+    """
     def __init__(self):
-        self.edges = {}
+        self.__edges = {}
         
     def add_node(self, node):
-        if node in self.edges:
+        if node in self.__edges:
             raise ValueError('Duplicate node')
         else:
-            self.edges[node] = []
-    
-    def add_edge(self, node):
-        src = node.get_src()
-        dest = node.get_dest()
+            self.__edges[node] = []
+            
+    def add_edge(self, edge):
+        src = edge.get_src()
+        dest = edge.get_dest()
         
-        if not (src in self.edges and dest in self.edges):
+        if not (src in self.__edges and dest in self.__edges):
             raise ValueError('Node not in graph')
             
-        self.edges[src].append(dest)
-        
+        else:
+            self.__edges[src].append(dest)
+            
     def child_of(self, node):
-        return self.edges[node]
+        return self.__edges[node]
     
     def has_node(self, node):
-        return node in self.edges
+        return node in self.__edges
     
     def get_node(self, name):
-        for node in self.edges:
+        for node in self.__edges:
             if node.get_name() == name:
                 return node
-        
-        raise NameError(name)
-        
+        else:
+            raise NameError(name)
+            
     def __str__(self):
         result = []
         
-        for src in self.edges:
-            for dest in self.edges[src]:
-                result.append(src.get_name() + '->'+ dest.get_name() + '\n')
-        
-        # omit final new line
-        result[-1] = result[-1][:-1]
-        
-        return ''.join(result)
+        for src in self.__edges:
+            for dest in self.__edges[src]:
+                result.append(src.get_name() + '->'\
+                         + dest.get_name() + '\n')
+                
+        return "".join(result)[:-1]
     
 class Graph(Digraph):
+    """
+    build graph by adding edges and their reversed ones using Digraph
+    """
     def add_edge(self, edge):
         Digraph.add_edge(self, edge)
-        rev = Edge(edge.get_dest(), edge.get_src())
-        Digraph.add_edge(self, rev)
-        
+        reversed_edge = Edge(edge.get_dest(), edge.get_src())
+        Digraph.add_edge(self, reversed_edge)
+    
 def build_city_graph(graph_type):
     g = graph_type()
     
-    names = ['Boston', 'Providence', 'New York', 'Chicago',
-             'Denver', 'Phoenix', 'Los Angeles']
-    
-    # create 7 nodes with each name
-    for name in names: 
+    for name in ('Boston', 'Providence', 'New York', 'Chicago',
+                 'Denver', 'Phoenix', 'Los Angeles'): #Create 7 nodes
         g.add_node(Node(name))
-        
+    
     g.add_edge(Edge(g.get_node('Boston'), g.get_node('Providence')))
     g.add_edge(Edge(g.get_node('Boston'), g.get_node('New York')))
     g.add_edge(Edge(g.get_node('Providence'), g.get_node('Boston')))
@@ -101,29 +106,12 @@ def build_city_graph(graph_type):
     g.add_edge(Edge(g.get_node('Denver'), g.get_node('Phoenix')))
     g.add_edge(Edge(g.get_node('Denver'), g.get_node('New York')))
     g.add_edge(Edge(g.get_node('Los Angeles'), g.get_node('Boston')))
+    
     return g
+
+
+print('\nDigraph:')
+print(build_city_graph(Digraph))
     
+print('Graph:')
 print(build_city_graph(Graph))
-        
-"""
-output:
-    
-    Boston->Providence
-    Boston->New York
-    Boston->Providence
-    Boston->Los Angeles
-    Providence->Boston
-    Providence->Boston
-    Providence->New York
-    New York->Boston
-    New York->Providence
-    New York->Chicago
-    New York->Denver
-    Chicago->New York
-    Chicago->Denver
-    Denver->Chicago
-    Denver->Phoenix
-    Denver->New York
-    Phoenix->Denver
-    Los Angeles->Boston
-"""
